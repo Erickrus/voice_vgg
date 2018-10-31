c2a7690eadaa6efe278ecf9066332e326e62eaf9
Erickrus <hyinghao@hotmail.com>
2018-10-31 13:13:35 +0800

    Update vvgg.py

--- vvgg.py
+++ vvgg.py
@@ -1,3 +1,10 @@
+# Some ideas inspired by this video, 
+# if it is possible to use a convolutional neural network approach
+# for the voice recognition application
+
+# Transfer Learning - Use Inception V3 to Solve Any ML Problem (Tempo Detection)
+# https://www.youtube.com/watch?v=jk0Qh7Iva_s
+
 import os
 import math
 import numpy as np
@@ -9,7 +16,9 @@ waves = [
   "morning.wav",
   "thanks.wav"
 ]
+
 os.system("mkdir output")
+
 MAX_SIZE = 20000
 SPLIT_NUM = 20
 for filename in waves:
@@ -19,14 +28,18 @@ for filename in waves:
   paddedWave = np.concatenate((wv[1], np.zeros(MAX_SIZE-waveLen)))
   splitSize = MAX_SIZE // SPLIT_NUM
   halfSplitSize = MAX_SIZE // SPLIT_NUM // 2
+  
   for i in range(SPLIT_NUM):
-
+    # split the wave into basic unit
     currWave = paddedWave[i*splitSize:(i+1)*splitSize]
     currFft  = np.fft.fft(currWave)
     currFreq = np.fft.fftfreq(currFft.size)
-    # get the positive part
+    
+    # get the positive part, and move the display position to next 0.5
     currFft  = currFft[:halfSplitSize] / 1e7
     currFreq = currFreq[:halfSplitSize] + i * 0.5
+    
+    # just plot the real part
     plt.plot(currFreq, currFft.real)
 
   plt.xlim(left=0, right=SPLIT_NUM * 0.5)
